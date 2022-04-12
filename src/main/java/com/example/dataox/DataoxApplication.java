a9d30dc8b034c5c96ec91b60b298705b38cf34f1
lusmem <zavyalova_la@magnit.ru>
2022-04-12 19:19:43 +0300

    Initial commit

---
+++ src/main/java/com/example/dataox/DataoxApplication.java
@@ -0,0 +1,13 @@
+package com.example.dataox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataoxApplication.class, args);
+    }
+
+}
